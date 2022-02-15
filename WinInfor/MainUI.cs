3144ba5034d0dd7764819d5b418bb2cc4a1b3155
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2022-02-16 01:20:04 +0700

    Update new version

--- WinInfor/MainUI.cs
+++ WinInfor/MainUI.cs
@@ -106,7 +106,7 @@ namespace WinInfor
 
                     if (latestVersion != appVersion)
                     {
-                        DialogResult dialogResult = MessageBox.Show("A new version has been released. Do you want to download it now?\n  Update Content: \n" + latestVersion, "Update Notification", MessageBoxButtons.YesNo);
+                        DialogResult dialogResult = MessageBox.Show("A new version has been released. Do you want to download it now?\nThis update package includes: \n" + latestVersion, "Update Notification", MessageBoxButtons.YesNo);
                         if (dialogResult == DialogResult.Yes)
                         {
                             Process.Start("https://github.com/phanxuanquang/WinInfor/releases/latest");
